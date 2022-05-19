6820703fd2966cefc61f43b055ce73fd56b5eac7
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-19 02:00:04 -0300

    WalletPage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+export function addEmail(email) {
+  return {
+    type: SAVE_EMAIL,
+    email,
+  };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,22 @@
+import { combineReducers } from 'redux';
+import wallet from './wallet';
+import user from './user';
+
+const rootReducer = combineReducers ({ user, wallet });
+
+export default rootReducer;
+
+/* const INITIAL_STATE = { name: '' }; */
+/*  */
+/* const reducer = (state = INITIAL_STATE, action) => { */
+/*   switch (action.type) { */
+/*   case 'CHANGE_NAME': */
+/*     return { ...state, name: action.payload }; */
+/*   default: */
+/*     return state; */
+/*   } */
+/* }; */
+
 // import user from './user';
 // import wallet from './wallet';
 
