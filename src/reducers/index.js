cc86438871a832c9f692b1b352fae7bd5557d2be
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-19 02:10:04 -0300

    WalletPage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import wallet from './wallet';
 import user from './user';
 
-const rootReducer = combineReducers ({ user, wallet });
+const rootReducer = combineReducers({ user, wallet });
 
 export default rootReducer;
 
