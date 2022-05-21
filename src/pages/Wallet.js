59e90b7a737cfe05f62c5364a178ee1e52b6805e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-21 03:04:27 -0300

    ExpensesTable

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -8,6 +8,22 @@ class Wallet extends React.Component {
       <section>
         <Header />
         <Form />
+
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
       </section>
     );
   }
